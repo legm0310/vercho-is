212053e395f7d43f00eabf7ae142376bf22dcc83
Lee gyu min <legm0310@gmail.com>
2021-12-13 20:41:39 +0900

    make it better

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // database: "mongodb://localhost:27017/ber_cho_is",
+  database: "mongodb+srv://Leegyumin:55NLzfnaFJccAR8@vercho-is-data.atlf8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secret: "thisisveryveryimportant-thatwoosangisfuckinpig",
+};
